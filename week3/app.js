f037e44138bdeb63a82998b95696d5428458ea99
ShyarK <50765996+ShyarK@users.noreply.github.com>
2019-06-08 23:01:53 +0200

    Update app.js

--- week3/app.js
+++ week3/app.js
@@ -1,12 +1,18 @@
+// Use the const keyword to declare variables that cannot be reassigned.
+// Here I selected all element, that I want to change thier state. 
 const menu = document.getElementById('menu-hamburger');
 const mainNav = document.getElementById('main-nav');
 const clearButton = document.getElementById('clear');
 const formInput = document.getElementById('search-keyword');
 const topMenu = document.getElementById('top-menu');
-
-
-
-// To add the close and drop-down class
+// The addEventlistener method attaches click event to document object (the div with the id of menu),
+// And it has two preameters:
+// 1- Type of the event like "click" or "onkeydown" and so many. 
+// 2- A  callback function.
+// when a user click on the menu, the callback function get excuted.
+// Inside callback function, if statement will check if the certain conditions, defined in the parentheses is true
+// If it is true the program executes the statement inside the curly braces. 
+// If false will go to the else statment. 
 menu.addEventListener("click", ()=> {    
     if (menu.classList.contains("close") && mainNav.classList.contains("drop-down")) {
          menu.classList.remove("close");
@@ -17,7 +23,10 @@ menu.addEventListener("click", ()=> {
     }
   });
 
-//Add class clear-active when form field has a value, and remove when doesn't
+//Add class clear-active when form field has a value, and remove when it doesn't.
+//Because the value of the form is a string we can find the length of the String 
+//by writing .length,  when it equals to 0 it means it doesn't have anything in the form field.
+// then the condition will be true.
 formInput.addEventListener("input", () => {
        clearButton.classList.add("clear-active");
     if(formInput.value.length === 0){
@@ -26,15 +35,23 @@ formInput.addEventListener("input", () => {
 });
 
 // Remove form field value
+// Here we check if the condition is ture
+// So when it is ture the  program executes the statement inside the curly braces.
 
 clearButton.addEventListener("click", () =>{
     if(formInput.value) {
         formInput.value = " ";
-        clearButton.removeAttribute("class");
+        clearButton.classList.remove("clear-active");
     }
 });
 
 // Add class onscroll
+// onscroll is an event accures when a user scroll the web browser.
+// The scrollY returns the number of pixels that the document is currently scrolled vertically.
+// When its value is 0 it means scroll position is in the top.
+// So when scrolling down will remove the calss
+// When scrolling up will add the class
+// When scrolling back to the top will remove the classes.
 
 let startScroll = 0;
 window.addEventListener('scroll', () => {
